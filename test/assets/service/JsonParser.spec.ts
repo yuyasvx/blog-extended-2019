dde8499615f1d800999148d82c200c12f856c7a2
yuyasvx <yuyasvx@gmail.com>
2019-07-13 19:39:58 +0900

    アサーション方法変更

--- test/assets/service/JsonParser.spec.ts
+++ test/assets/service/JsonParser.spec.ts
@@ -2,7 +2,6 @@ import { pipe } from 'fp-ts/lib/pipeable'
 import * as either from 'fp-ts/lib/Either'
 import { toBlogEntry, parseToBlogEntry } from '@/assets/service/JsonParser'
 // import * as blogEntry from '@/assets/interface/BlogEntry'
-import '@/assets/auto-config/dayjs'
 
 describe('toBlogInfo', () => {
   it('正常なデータをパースする', () => {
@@ -25,7 +24,8 @@ describe('toBlogInfo', () => {
 
     const result = toBlogEntry(data)
     expect(result.title).toBe('test data')
-    expect(result.date.locale('ja').format()).toBe('2017-03-05T21:10:54+09:00')
+    expect(result.date.isValid()).toBe(true)
+    expect(result.date.toISOString()).toBe('2017-03-05T12:10:54.000Z')
     // pipe(
     //   toBlogEntry(data),
     //   either.fold(
@@ -135,22 +135,30 @@ describe('parseToBlogInfo', () => {
     const jsonData = {
       data: [
         {
-          title: 'Docker初挑戦メモ',
+          title: 'test title 1',
           subtitle: null,
           date: '2019-03-16 00:16:03 +0900 JST',
           categories: [{ name: 'Development', link: 'development' }],
-
           tags: [{ name: 'Docker', link: 'docker' }, { name: '雑記', link: '%E9%9B%91%E8%A8%98' }],
           jsonlink: 'https://blog.yuyasvx.me/post/2019/03/hello-docker/index.json',
           permalink: '/post/2019/03/hello-docker/',
           hasCoverImage: false
         },
         {
-          title: 'Docker初挑戦メモ2',
+          title: 'test title 2',
           subtitle: null,
           date: '2019-03-16 01:16:03 +0900 JST',
           categories: [{ name: 'Development', link: 'development' }],
-
+          tags: [{ name: 'Docker', link: 'docker' }, { name: '雑記', link: '%E9%9B%91%E8%A8%98' }],
+          jsonlink: 'https://blog.yuyasvx.me/post/2019/03/hello-docker/index.json',
+          permalink: '/post/2019/03/hello-docker/',
+          hasCoverImage: false
+        },
+        {
+          title: 'test title 3',
+          subtitle: null,
+          date: '2019-03-16 01:16:03 +0900 JST',
+          categories: [{ name: 'Development', link: 'development' }],
           tags: [{ name: 'Docker', link: 'docker' }, { name: '雑記', link: '%E9%9B%91%E8%A8%98' }],
           jsonlink: 'https://blog.yuyasvx.me/post/2019/03/hello-docker/index.json',
           permalink: '/post/2019/03/hello-docker/',
@@ -168,7 +176,11 @@ describe('parseToBlogInfo', () => {
         err => {
           throw err
         },
-        () => {
+        data => {
+          expect(data.length).toBe(3)
+          data.forEach((d, i) => {
+            expect(d.title).toBe(jsonData.data[i].title)
+          })
           done()
         }
       )
